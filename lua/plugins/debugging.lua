9b39fedac210d295d5176e058543d0907f18eb1b
Doug Nichols <doug16@gmail.com>
2024-02-04 07:06:53 -0500

    Added the base requirements for DAP (debugging)

---
+++ lua/plugins/debugging.lua
@@ -0,0 +1,28 @@
+return {
+	'mfussenegger/nvim-dap',
+	dependencies = { 'rcarriga/nvim-dap-ui' },
+	config = function()
+		local dap = require("dap")
+		local dapui = require('dapui')
+
+		dapui.setup()
+
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+
+		vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, {})
+		vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+		vim.keymap.set("n", "<Leader>ds", dap.step_over, {})
+		vim.keymap.set("n", "<Leader>dS", dap.step_into, {})
+	end
+}
