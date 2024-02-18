1c3adb598cc7c90da03304254f6e80189eaba742
Doug Nichols <doug16@gmail.com>
2024-02-18 09:42:21 -0500

    Cleanup autopairs config

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -26,11 +26,7 @@ return {
 	{
 		'windwp/nvim-ts-autotag',
 		config = function()
-			require('nvim-ts-autotag').setup({
-				filetypes = {
-					'html', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'svelte', 'vue', 'tsx',
-					'jsx', 'rescript', 'xml', 'php', 'markdown', 'astro', 'glimmer', 'handlebars', 'hbs', 'jinja' }
-			})
+			require('nvim-ts-autotag').setup()
 		end
 	},
 }
