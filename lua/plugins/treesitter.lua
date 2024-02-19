03857dd48da05583b3bb43a6062828d81f0bcc19
Nichols, Doug <doug.nichols@ge.com>
2024-02-19 09:07:02 -0500

    Add vimdoc and query to treesitter ensure installed

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,13 +4,23 @@ return {
 	config = function()
 		require('nvim-treesitter.configs').setup {
 			-- A list of parser names, or "all"
-			ensure_installed = { "c", "lua", "rust", "vim", "html", "css", "javascript", "python" },
+			ensure_installed = { "c", "lua", "rust", "vim", "html", "css",
+				"javascript", "python", "vimdoc", "query" },
 
 			-- Install parsers synchronously (only applied to 'ensure_installed')
 			sync_install = false,
 			auto_install = true,
 			highlight = { enable = true, },
 			indent = { enable = true, },
+			incremental_selection = {
+				enable = true,
+			keymaps = {
+				init_selection = "<Leader>ss",
+				node_incremental = "<Leader>si",
+				scope_incremental = "<Leader>s>",
+				node_decremental = "<Leader>sd",
+			},
+			},
 		}
 	end
 }
