1221e647bcf982c12c491d4e9795b56b93e239d7
Doug Nichols <doug16@gmail.com>
2024-02-20 20:46:46 -0500

    Add nabla plugin for displaying math

---
+++ lua/plugins/nabla.lua
@@ -0,0 +1,12 @@
+return {
+	"jbyuki/nabla.nvim",
+	config = function()
+		local nabla = require("nabla")
+		nabla.enable_virt({
+			autogen = true, -- auto-regenerate ASCII art when exiting insert mode
+			silent = true, -- silents error messages
+		})
+		-- Setup a keybinding
+		vim.keymap.set('n', '<Leader>m', require("nabla").popup, { remap = false })
+	end,
+}
