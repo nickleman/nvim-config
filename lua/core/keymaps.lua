5e0351b90e4e47979a69691853b1b0b005cd2c76
Nichols, Doug <doug.nichols@ge.com>
2024-04-01 12:36:20 -0400

    Add some diagnostic and highlight keymaps.

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -33,3 +33,13 @@ vim.api.nvim_set_keymap("n", "<Leader>tp", ":tabprevious<Cr>", { noremap = true 
 vim.api.nvim_set_keymap('i', '<C-H>', '<C-w>', { noremap = true })
 -- Control-Delete to end of word
 vim.api.nvim_set_keymap('i', '<C-Del>', '<Esc>lce', { noremap = true })
+
+-- Set highlight on search, but clear on pressing <Esc> in normal mode
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
