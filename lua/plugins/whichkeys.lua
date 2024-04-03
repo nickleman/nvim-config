4cd0b9271c5432ec3f316a42871d3c44fb4e9190
Doug Nichols <doug16@gmail.com>
2024-04-02 20:15:52 -0400

    Register more key groupings with whichkey

--- lua/plugins/whichkeys.lua
+++ lua/plugins/whichkeys.lua
@@ -6,11 +6,15 @@ return  { -- Useful plugin to show you pending keybinds.
 
       -- Document existing key chains
       require('which-key').register {
-        ['<leader>c'] = { name = '[c]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[d]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[r]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[f]uzzy find', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[w]orkspace', _ = 'which_key_ignore' },
+        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+        ['<leader>f'] = { name = '[F]uzzy Find', _ = 'which_key_ignore' },
+        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+        ['<leader>h'] = { name = 'Gitsign Change [H]unks', _ = 'which_key_ignore' },
+        ['<leader>t'] = { name = '[T]abs', _ = 'which_key_ignore' },
+        ['<leader>s'] = { name = '[S]election', _ = 'which_key_ignore' },
+        ['<leader>g'] = { name = '[G]it Worktrees', _ = 'which_key_ignore' },
       }
     end,
   }
