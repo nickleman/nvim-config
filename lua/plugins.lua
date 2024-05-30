f82d3f0473ae1a2404ccdee1419769910d0278ee
Doug Nichols <doug16@gmail.com>
2024-05-30 17:27:03 -0400

    fix formatting

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,14 +1,14 @@
 return {
-	-- 'tpope/vim-surround',  -- Surrounding ysw)
-	'tpope/vim-commentary', -- For Commenting gcc & gc
-	'ap/vim-css-color',    -- CSS Color Preview
-	-- 'rafi/awesome-vim-colorschemes', -- Retro Scheme
-	'tc50cal/vim-terminal', -- Vim Terminal
-	'preservim/tagbar',    -- Tagbar for code navigation
-	'mg979/vim-visual-multi', -- <C-Down> for multiple cursors
-	-- 'neovim/nvim-lspconfig', -- LSP config package
-	-- Auto completion
-	-- LSP configuration and utilization
+    -- 'tpope/vim-surround',  -- Surrounding ysw)
+    'tpope/vim-commentary', -- For Commenting gcc & gc
+    'ap/vim-css-color',    -- CSS Color Preview
+    -- 'rafi/awesome-vim-colorschemes', -- Retro Scheme
+    'tc50cal/vim-terminal', -- Vim Terminal
+    'preservim/tagbar',    -- Tagbar for code navigation
+    'mg979/vim-visual-multi', -- <C-Down> for multiple cursors
+    -- 'neovim/nvim-lspconfig', -- LSP config package
+    -- Auto completion
+    -- LSP configuration and utilization
 
-	'Glench/Vim-Jinja2-Syntax', -- Jinja 2 syntax highlighting
+    'Glench/Vim-Jinja2-Syntax', -- Jinja 2 syntax highlighting
 }
