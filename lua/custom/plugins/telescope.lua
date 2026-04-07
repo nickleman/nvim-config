cec7c41be32f6a3c04c917c98483e78caab0a526
Doug Nichols <doug16@gmail.com>
2026-04-06 22:04:58 -0400

    Updated for latest nvim-treesitter and neovim versions

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -1,6 +1,7 @@
 return {
     'nvim-telescope/telescope.nvim', -- Alternate file browser
-    branch = '0.1.x',
+    -- branch = '0.1.x',
+    branch = 'master',
     dependencies = {
         'nvim-lua/plenary.nvim',
 
--- lua/custom/treesitter.lua
+++ lua/custom/treesitter.lua
@@ -1,7 +1,7 @@
 if vim.loop.os_uname().sysname == "Windows_NT" then
     require('nvim-treesitter.install').compilers = { "clang" }
 end
-require('nvim-treesitter.configs').setup {
+require('nvim-treesitter.config').setup {
     -- A list of parser names, or "all"
     ensure_installed = { "c", "lua", "rust", "vim", "html", "css",
         "javascript", "python", "vimdoc", "query", "latex", "sql" },
