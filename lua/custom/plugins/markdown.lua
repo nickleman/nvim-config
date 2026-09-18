5bb598ade70bb92fea397465f59940633a74c129
Doug Nichols <doug16@gmail.com>
2026-09-18 08:11:45 -0400

    Replace markdown-preview with actively maintained fork.

--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -1,12 +1,22 @@
 return {
     {
-        "iamcco/markdown-preview.nvim",
+        "selimacerbas/markdown-preview.nvim",
         lazy = true,
         cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
         ft = { "markdown" },
-        build = function()
-            vim.fn["mkdp#util#install"]()
-        end,
+        dependencies = {
+            "selimacerbas/live-server.nvim"
+        },
+        config = function()
+            require("markdown_preview").setup({
+                -- All optional config settings (sane defaults shown):
+                instance_mode = "takeover", -- "takeover" (one tab) or "multi" (tab per instance)
+                port = 0,           -- 0 = auto-assign
+                open_browser = true,
+                default_theme = "light", -- "dark" or "light"
+                debounce_ms = 300,
+            })
+        end
     },
     {
         'jakewvincent/mkdnflow.nvim',
@@ -17,3 +27,4 @@ return {
         end
     },
 }
+
